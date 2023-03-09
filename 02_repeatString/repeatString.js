8af5c071cce684a822e70906ca4505d5c1241b0e
mojoee <moritzsontheimer@web.de>
2023-03-09 13:06:34 +0800

    solved 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(input_string, n) {
+if (n<0){
+return "ERROR"
+}
+let result=""
+for (let i=n; i>0; i--){
+    result+=input_string
+}
+return result
 };
 
 // Do not edit below this line

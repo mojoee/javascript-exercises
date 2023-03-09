a96b76b02aa6e13ae1aca940c6e5b7b605e199ec
mojoee <47809126+mojoee@users.noreply.github.com>
2023-03-09 22:03:24 +0800

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(lower, upper) {
+    if (!(Number.isInteger(lower) && Number.isInteger(upper))){return "ERROR";}
+    if (lower<0 || upper<0){return "ERROR";}
+    if (lower>upper) {
+        let store=lower
+        lower=upper
+        upper=store
+    }
+    let sum = 0;
+    for (let index = lower; index <= upper; index++) {
+        sum+=index;
+        
+    }
+    return sum
 
 };
 

47fccea3121e560dbd11cb7dffd4a50c8eb33121
mojoee <47809126+mojoee@users.noreply.github.com>
2023-03-09 20:56:01 +0800

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    var arrayLength = args.length
+    for (var i=0; i<arrayLength; i++){
+    var index = arr.indexOf(args[i])
+    if (index===-1){continue;}
+    arr.splice(index, 1)
+    }
 
+    return arr
 };
 
 // Do not edit below this line
